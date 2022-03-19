537c9cca3f53c8c77862e73e0142de3741b1eaed
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-03-19 09:07:14 -0300

    feat: exercise 1

---
+++ arithmeticOperations.js
@@ -0,0 +1,15 @@
+// EXERCÍCIO 1
+// Faça as 4 operações básica con base nos números 2 e 4:
+// Ao final de cada operação imprina na tela no seguinte fornato:
+// Soma: 2 + 4 = x
+// Subtração : 2 - 4 = x
+// Divisão: 2/4 = x
+// Multiplicação: 2 * 4 = x
+// --
+
+let num1 = 2;
+let num2 = 4;
+console.log('Soma: 2 + 4 = ' + (num1 + num2));
+console.log('Soma: 2 - 4 = ' + (num1 - num2));
+console.log('Soma: 2 / 4 = ' + (num1 / num2));
+console.log('Soma: 2 * 4 = ' + (num1 * num2));
