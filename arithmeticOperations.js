f41c2fe8a09a0d310f03dd8d1c88f99258fa9bcc
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-03-19 09:19:47 -0300

    feat: exercise 1 - my devagation

--- arithmeticOperations.js
+++ arithmeticOperations.js
@@ -1,6 +1,7 @@
 // EXERCÍCIO 1
 // Faça as 4 operações básica con base nos números 2 e 4:
 // Ao final de cada operação imprina na tela no seguinte fornato:
+
 // Soma: 2 + 4 = x
 // Subtração : 2 - 4 = x
 // Divisão: 2/4 = x
@@ -9,7 +10,19 @@
 
 let num1 = 2;
 let num2 = 4;
-console.log('Soma: 2 + 4 = ' + (num1 + num2));
-console.log('Soma: 2 - 4 = ' + (num1 - num2));
-console.log('Soma: 2 / 4 = ' + (num1 / num2));
-console.log('Soma: 2 * 4 = ' + (num1 * num2));
+let arr = ['+', '-', '*', '/']
+
+function sum(num1, num2) {
+  console.log({
+    soma: num1 + num2,
+    subt: num1 - num2,
+    multi: num1 * num2,
+    divisao: num1 / num2,
+  });
+}
+
+sum(5, 8)
+// console.log('Soma: 2 + 4 = ' + (num1 + num2));
+// console.log('Soma: 2 - 4 = ' + (num1 - num2));
+// console.log('Soma: 2 / 4 = ' + (num1 / num2));
+// console.log('Soma: 2 * 4 = ' + (num1 * num2));
